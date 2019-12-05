3e37063dbeffc0810b03587210f6b10da6a645fb
tokinh0 <jean-recados@outlook.com>
2019-12-05 14:23:36 -0300

    changing database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_05_163150) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accesses", force: :cascade do |t|
     t.string "longitude"
     t.string "latitude"
